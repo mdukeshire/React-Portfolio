de5d256db6ef5901b788c88d5149ef848c5d9629
Matthew Dukeshire <mdukeshire07@gmail.com>
2021-12-16 22:02:12 -0500

    Updates

---
+++ precache-manifest.8496960b259d626d0f2904e16ff0dd5b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca33699bad54a32e713cee7f8a920dcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09cc25c7009b7487919b",
+    "url": "/static/css/main.48ead0a4.chunk.css"
+  },
+  {
+    "revision": "ec7dea27f2d267470dff",
+    "url": "/static/js/2.102548e6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.102548e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09cc25c7009b7487919b",
+    "url": "/static/js/main.ea1aecef.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "8b7147443e2ac4b537b4e17963c44c4b",
+    "url": "/static/media/Password-Generator.8b714744.jpg"
+  },
+  {
+    "revision": "5b7a694c61291ebd489c4a077ece5ff3",
+    "url": "/static/media/Planmark.5b7a694c.jpg"
+  },
+  {
+    "revision": "f6922d86318832133b15a4bc054cd063",
+    "url": "/static/media/Weather-Dashboard.f6922d86.jpg"
+  },
+  {
+    "revision": "a39e457642c143220b04ea0416f79d8e",
+    "url": "/static/media/Workout-Tracker.a39e4576.jpg"
+  },
+  {
+    "revision": "402a471b5aad190a33b649f1368f5de1",
+    "url": "/static/media/cover-image.402a471b.jpg"
+  }
+]);
